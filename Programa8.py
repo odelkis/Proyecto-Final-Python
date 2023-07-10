d7f51be1043acddd44e4504111610a4b1c0dc8c0
odelkis <138025592+odelkis@users.noreply.github.com>
2023-07-09 22:41:38 -0500

    Update Programa8.py

--- Programa8.py
+++ Programa8.py
@@ -10,10 +10,10 @@ D = (6 * x) + 4 +(8 * x) + 2
 E = ((5 * x) + 78) / 28
 F = (((6 * x) - 7) / 4) + (((3 * 7) -5)/7)
 
-print("El resulatdo de A es ",round(A,2))
-print("El resulatdo de B es ",round(B,2))
-print("El resulatdo de C es ",round(C,2))
-print("El resulatdo de D es ",round(D,2))
-print("El resulatdo de E es ",round(E,2))
-print("El resulatdo de F es ",round(F,2))
+print("El resultado de A es ",round(A,2))
+print("El resultado de B es ",round(B,2))
+print("El resultado de C es ",round(C,2))
+print("El resultado de D es ",round(D,2))
+print("El resultado de E es ",round(E,2))
+print("El resultado de F es ",round(F,2))
 
